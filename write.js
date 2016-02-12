de65aa425322a19c6d730fbf98bc65d0b13e6891
Jason Weakley <jason.b.weakley@gmail.com>
2016-02-12 10:34:24 -0600

    added files and basic code

---
+++ write.js
@@ -0,0 +1,9 @@
+// Create multiple IIFEs, following the Single Responsibility Principle, that perform the following functions. The name of your global variable that gets augmented by the IIFEs should be Chatty.
+
+// One IIFE should load the JSON file and returns the array of objects.
+// One IIFE should accept an element id, and the user message, and then add the user's message - along with the delete button - to the specified parent element. Each message should be stored in a private array in this IIFE. This IIFE should expose a function to read all messages, and delete a single message.
+// One IIFE should accept a message element id and then remove the correct element from the DOM. This IIFE should also remove the corresponding message from the private array that was created in the previous IIFE.
+
+
+
+
